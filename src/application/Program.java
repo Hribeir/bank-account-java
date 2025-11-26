793dcd6a422eab17c5d4458f590821c821357935
Henrique <hribeir6@gmail.com>
2025-11-25 22:34:21 -0300

    add constructors, overloading and encapsulation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,37 @@
 package application;
 
+import entities.BankAccount;
+
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Program {
     public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
 
+        System.out.print("Enter account number: ");
+        int numberAccount = sc.nextInt();
+        sc.nextLine();
+        System.out.print("Enter account holder: ");
+        String holder = sc.nextLine();
+        System.out.print("Is there na initial deposit (y/n)? ");
+        char depositOption = sc.next().charAt(0);
+
+
+        if (depositOption == 'y') {
+            System.out.print("Enter initial deposit value: ");
+            double initialDeposit = sc.nextDouble();
+            BankAccount bankAccount = new BankAccount(numberAccount, holder, depositOption, initialDeposit);
+            System.out.println();
+            System.out.println("Account data:");
+            System.out.print(bankAccount.toString());
+        } else {
+            BankAccount bankAccount = new BankAccount(numberAccount, holder, depositOption);
+            System.out.println();
+            System.out.print(bankAccount.toString());
+        }
+
+        sc.close();
     }
 }
--- src/entities/BankAccount.java
+++ src/entities/BankAccount.java
@@ -1,5 +1,40 @@
 package entities;
 
 public class BankAccount {
+    private final int numberAccount;
+    private  final String holder;
+    private final char depositOption;
+    private  double initialDeposit;
 
+    public BankAccount(int numberAccount, String holder, char depositOption, double initialDeposit) {
+        this.numberAccount = numberAccount;
+        this.holder = holder;
+        this.depositOption = depositOption;
+        this.initialDeposit = initialDeposit;
+    }
+    public BankAccount(int numberAccount, String holder, char depositOption) {
+        this.numberAccount = numberAccount;
+        this.holder = holder;
+        this.depositOption = depositOption;
+    }
+
+
+    public int getNumberAccount() {
+        return numberAccount;
+    }
+
+    public String getHolder() {
+        return holder;
+    }
+
+    public double getInitialDeposit() {
+        return initialDeposit;
+    }
+
+    public String toString() {
+        return
+        "Account "  + getNumberAccount()
+        + ", Holder: " + getHolder()
+        + ", Balance: $ " + getInitialDeposit();
+    }
 }
